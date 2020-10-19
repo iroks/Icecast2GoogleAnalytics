ef1af2c1d21f3293201707f82059f73b48b25907
igor <info@coherent-receiver.com>
2020-10-19 14:05:02 +0200

    added a simple cache by Icecast requests

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
 # Icecast2GoogleAnalytics
-v1
+<h2>v1</h2>
 https://coherent-receiver.com/google-analytics-and-icecast - for more details <br /> 
 https://github.com/ned-kelly/docker-icecast-google-analytics - dockerised version  <br />
 https://coherent-receiver.com/multi-channel-receivers-for-radio-station-monitoring - hardware platform for broadcasting monitoring, transcoding or hybrid radio <br />
 
 Please don't hesitate to contact us if you have additional requirements, wishes or ideas 
+<br />
+<h2>v2</h2>
+<h3>Dependencies</h3>
+Grafana:
+docker run -d -p 3000:3000 grafana/grafana <br />
+InfluxDB: docker run -p 8086:8086  -v $PWD/influxdb:/var/lib/influxdb:rw influxdb <br />
+<br>
+
+influx <br />
+Connected to http://localhost:8086 version 1.8.3 <br />
+InfluxDB shell version: 1.6.4 <br />
+\> use icecast <br>
+Using database icecast <br />
+\> select * from icecast_listeners <br>
+name: icecast_listeners <br />
+time                location num_listeners server <br />
+----                -------- ------------- ------ <br />
+1602847612118000000 /live    15568           http://.....
+
+
+HLS: ffmpeg -i http:/... -c:a aac -b:a 128k -ac 2 -f hls -hls_time 4 -hls_playlist_type event stream.m3u8cd /var/
--- src/main/java/com/coherentreceiver/analytics/task/AbstractTask.java
+++ src/main/java/com/coherentreceiver/analytics/task/AbstractTask.java
@@ -9,6 +9,9 @@ import com.coherentreceiver.analytics.ga.service.GAService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.time.Instant;
 
 public abstract class AbstractTask {
 
@@ -21,24 +24,41 @@ public abstract class AbstractTask {
     @Autowired
     protected ServerService serverService;
 
+    @Value ("#{appconfiguration.icecastUpdateFrequency}")
+    protected long icecastUpdateFrequency;
+
+    protected Listeners listeners;
+    protected StreamProperty streamProperty;
+    protected long prevTimeStampSeconds = 0;
 
     public void updateTask() {
-        logger.info("start influxdb process");
-        config.getServers()
-                .stream()
-                .map(server -> serverService.getStreamPropertiesByServer(server))
-                .forEach(streamProperty -> streamProperty.forEach(sp -> {
-                    streamPropertyService.setStreamProperty(sp);
-                    Listeners listeners = streamPropertyService.getListeners();
-                    //there are no listeners anymore
-                    if (listeners == null) {
-                        return;
-                    }
-                    updateSynchronous(sp, listeners);
-                }));
+        logger.info("start update process");
 
+        Instant instant = Instant.now();
+        long currTimeStampSeconds = instant.getEpochSecond();
+        if ( (currTimeStampSeconds - prevTimeStampSeconds) > icecastUpdateFrequency) {
+            logger.info ("processing by triggering Icecast request");
+            config.getServers()
+                    .stream()
+                    .map(server -> serverService.getStreamPropertiesByServer(server))
+                    .forEach(streamProperty -> streamProperty.forEach(sp -> {
+                        streamPropertyService.setStreamProperty(sp);
+                        Listeners listeners = streamPropertyService.getListeners();
+                        //there are no listeners anymore
+                        if (listeners == null) {
+                            return;
+                        }
+                        this.listeners = listeners;
+                        this.streamProperty = sp;
+                        this.prevTimeStampSeconds=currTimeStampSeconds;
+                        updateSynchronous(sp, listeners);
+                    }));
+
+        } else {
+            logger.info ("processing using Icecast cache");
+            updateSynchronous(this.streamProperty, this.listeners);
+        }
         logger.info("finish influxdb process");
-
     }
 
     public abstract void updateSynchronous(StreamProperty sp, Listeners listeners);
